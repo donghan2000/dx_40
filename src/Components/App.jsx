22c46a8d1d4daca87f11319d01673e0aa0dfd048
2501989 <2501989@sit.singaporetech.edu.sg>
2026-02-28 20:41:12 +0800

    Fourth Commit

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,31 +1,23 @@
-import Masthead from './Masthead.jsx'
-import Sidebar from './Sidebar.jsx';
-import Gallery from './Gallery.jsx';
-import Story from './Story.jsx';
-import Experience from './Experience.jsx';
-import Contact from './Contact.jsx';
-import Copyright from './Copyrights.jsx';
-
-
-
+import Masthead from "./Masthead.jsx";
+import Sidebar from "./Sidebar.jsx";
+import Gallery from "./Gallery.jsx";
+import Story from "./Story.jsx";
+import Experience from "./Experience.jsx";
+import Contact from "./Contact.jsx";
+import Copyright from "./Copyrights.jsx";
 
 function App() {
-
-  return <>
-
-    <Sidebar />
-    <Masthead />
-    <Gallery />
-    <Story/>
-    <Experience />
-    <Contact />
-    <Copyright />
-  </>
-
-
-
+  return (
+    <>
+      <Sidebar />
+      <Masthead />
+      <Gallery />
+      <Story />
+      <Experience />
+      {/* <Contact /> */}
+      <Copyright />
+    </>
+  );
 }
-
-
 
 export default App;
--- src/Components/Story.jsx
+++ src/Components/Story.jsx
@@ -1,76 +1,90 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { useInView } from 'react-intersection-observer';
-import RoCanvas from './Assets/Robot-Canvas.jsx';
-
-
+import { useInView } from "react-intersection-observer";
+import RoCanvas from "./Assets/Robot-Canvas.jsx";
 
 export default function Story() {
+  const [canvasRef, inView] = useInView({ threshold: 0 });
 
-    const [canvasRef, inView] = useInView({ threshold: 0 });
+  const [addClass, setAddClass] = useState(false);
 
-    const [addClass, setAddClass] = useState(false);
+  const handleScroll = () => {
+    if (window.scrollY > 1500) {
+      setAddClass(true);
+    }
+  };
 
-    const handleScroll = () => {
-        if (window.scrollY > 1500) {
-            setAddClass(true);
-        }
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
     };
+  }, []);
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
+  return (
+    <>
+      <section className="section-dividers" id="story">
+        <div className="crosses-l">
+          <div className="horizontal story-h"></div>
+          <div className="vertical story-v"></div>
+        </div>
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+        <div ref={canvasRef} className="about-div ">
+          <div className="about-text">
+            <div className="about-p">
+              <header
+                className={`${addClass ? "animate__animated animate__fadeIn animate__delay-1s" : ""}`}
+              >
+                My Story
+              </header>
+              <div
+                className={`behind-title-left ${addClass ? "animate__animated animate__fadeIn animate__delay-1s" : ""}`}
+              >
+                <p>Myself</p>
+              </div>
 
-    return <>
-        <section className='section-dividers' id="story">
-            <div className='crosses-l'>
-                <div className='horizontal story-h'></div>
-                <div className='vertical story-v'></div>
+              <div className="about-bubble-p">
+                <p
+                  className={`about-p-left ${addClass ? "animate__animated animate__jackInTheBox animate__delay-1s" : ""}`}
+                >
+                  Welcome to my portfolio site. I am an aspiring junior web
+                  developer with a passion for the field. My expertise is in
+                  Javascript and CSS, and I am currently learning React and Node
+                  frameworks. I have basic knowledge of PHP and SQL.
+                </p>
+
+                <p
+                  className={`about-p-right ${addClass ? "animate__animated animate__jackInTheBox animate__delay-2s" : ""}`}
+                >
+                  My hobbies include playing games, reading light novels, and
+                  watching anime. I recently started learning Japanese on my own
+                  with the goal of gaining a basic understanding.
+                </p>
+
+                <p
+                  className={`about-p-left ${addClass ? "animate__animated animate__jackInTheBox animate__delay-3s" : ""}`}
+                >
+                  {" "}
+                  I am continuously learning new web development techniques and
+                  look forward to collaborating with those reading this.
+                </p>
+              </div>
             </div>
+          </div>
 
-            <div ref={canvasRef} className='about-div '>
-                <div className='about-text'>
-
-                    <div className='about-p'>
-
-                        <header className={`${addClass ? 'animate__animated animate__fadeIn animate__delay-1s' : ''}`}>My Story</header>
-                        <div className={`behind-title-left ${addClass ? 'animate__animated animate__fadeIn animate__delay-1s' : ''}`}>
-                            <p>Myself</p>
-                        </div>
-
-                        <div className='about-bubble-p'>
-
-                            <p className={`about-p-left ${addClass ? 'animate__animated animate__jackInTheBox animate__delay-1s' : ''}`}>Welcome to my portfolio site. I am an aspiring junior web developer with a passion for the field. My expertise is in Javascript and CSS, and I am currently learning React and Node frameworks.
-                                I have basic knowledge of PHP and SQL.
-                            </p>
-
-                            <p className={`about-p-right ${addClass ? 'animate__animated animate__jackInTheBox animate__delay-2s' : ''}`}>My hobbies include playing games, reading light novels, and watching anime. I recently started learning Japanese on my own with the goal of gaining a basic understanding.
-                            </p>
-
-                            <p className={`about-p-left ${addClass ? 'animate__animated animate__jackInTheBox animate__delay-3s' : ''}`}> I am continuously learning new web development techniques and look forward to collaborating with those reading this.
-                            </p>
-
-                        </div>
-                    </div>
-                </div>
-
-                <div className={`about-canvas ${addClass ? 'animate__animated animate__fadeIn' : ''}`}>
-                    {inView && (
+          <div
+            className={`about-canvas ${addClass ? "animate__animated animate__fadeIn" : ""}`}
+          >
+            {/* {inView && (
                         <Canvas className={`${addClass ? 'animate__animated animate__fadeIn  animate__delay-1s' : ''}`}>
                             <ambientLight />
                             <RoCanvas />
                         </Canvas>
-                    )}
-                </div>
-            </div>
-
-
-
-        </section>
+                    )} */}
+          </div>
+        </div>
+      </section>
     </>
-
+  );
 }
